53d7b73a6a25afaf9a018643db092d1817174a81
YiJian <879614047@QQ.com>
2019-07-25 13:56:19 +0800

    lei

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
@@ -0,0 +1,7 @@
+package com.leyou.item.controller;
+
+/**
+ * Created by 87961 on 2019/7/25.
+ */
+public class CategoryController {
+}
---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.leyou.item.service;
+
+/**
+ * Created by 87961 on 2019/7/25.
+ */
+public class CategoryService {
+}
