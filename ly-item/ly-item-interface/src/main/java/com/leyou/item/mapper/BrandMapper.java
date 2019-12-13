5c0b36d27604ca035257d2ed48efe08dd7a51fee
YiJian <879614047@QQ.com>
2019-12-13 15:54:06 +0800

    商品分类

---
+++ ly-item/ly-item-interface/src/main/java/com/leyou/item/mapper/BrandMapper.java
@@ -0,0 +1,8 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Brand;
+import org.springframework.stereotype.Component;
+import tk.mybatis.mapper.common.Mapper;
+@Component
+public interface BrandMapper extends Mapper<Brand> {
+}
--- ly-item/ly-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
@@ -5,17 +5,17 @@ import com.leyou.item.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 /**
  * Created by 87961 on 2019/7/25.
  */
-@Controller
+@RestController
 @RequestMapping("category")
 public class CategoryController {
 
